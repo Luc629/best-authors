7eb42e48fa922e1a37a52352017041698c41cd45
Lucy629 <waigwe@hotmail.com>
2025-08-21 17:46:03 -0400

    Header components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import './App.css'
+import Header from './assets/components/header/Header'
 
 function App () {
-  return 
-      <div>test
-      </div>
-      
+  return (
+    <div className= "page-container">
+      <Header />
+    
+    
+    
+    </div>
+  )
+ 
 
 }
   
-  
-
-
 
 export default App
---
+++ src/assets/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+const Header = function  () {
+    return <div className= "Header">
+            <div className= "Heading">
+                <h1>Best Authors</h1>
+            </div>
+
+
+    </div>
+}
+
+export default Header
+    
